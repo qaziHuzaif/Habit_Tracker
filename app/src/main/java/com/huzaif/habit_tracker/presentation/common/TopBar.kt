22322bb5cb063266271ebddde5d31a9c09457b4c
qaziHuzaif <qazihuzaif25@gmail.com>
2025-08-04 14:48:56 +0530

    Refactored `TopBar` to include optional navigation icon.
    The following changes were made:
    - Added `isNavigationIconVisible` and `navController` parameters to the `TopBar` composable.
    - If `isNavigationIconVisible` is true and a `navController` is provided, a back arrow icon button is displayed.
    - Clicking the back arrow icon navigates up the back stack.
    - The title font size was changed from 28.sp to 26.sp.
    - The title now supports horizontal scrolling for long texts and is limited to a single line.

--- app/src/main/java/com/huzaif/habit_tracker/presentation/common/TopBar.kt
+++ app/src/main/java/com/huzaif/habit_tracker/presentation/common/TopBar.kt
@@ -1,24 +1,53 @@
 package com.huzaif.habit_tracker.presentation.common
 
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar(modifier: Modifier = Modifier, title: String) {
+fun TopBar(
+    modifier: Modifier = Modifier,
+    title: String,
+    isNavigationIconVisible: Boolean = false,
+    navController: NavHostController? = null
+) {
     TopAppBar(
         title = {
             Text(
                 text = title,
                 fontWeight = FontWeight.Medium,
-                fontSize = 28.sp
+                fontSize = 26.sp,
+                maxLines = 1,
+                modifier = Modifier
+                    .horizontalScroll(rememberScrollState())
             )
         },
-        modifier = modifier
+        modifier = modifier,
+        navigationIcon = {
+            if (isNavigationIconVisible && navController != null) {
+                IconButton(
+                    onClick = {
+                        navController.navigateUp()
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+            }
+        }
     )
 }
