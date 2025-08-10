857421e81558359f8360b66c015d9806ca413b39
qaziHuzaif <qazihuzaif25@gmail.com>
2025-08-11 03:15:56 +0530

    Implemented ignore battery optimization permission request.
    The following changes were made:
    - Added a check for battery optimization status in `MainActivity.kt`.
    - Implemented `RequestBatteryPermission` composable function to display an `AlertDialog` guiding the user to allow background activity if battery optimization is not ignored.
    - The dialog provides a button to navigate the user directly to the app's settings page.
    - A toast message is shown if the user dismisses the dialog, reminding them to allow background activity for reliable reminders.

--- app/src/main/java/com/huzaif/habit_tracker/MainActivity.kt
+++ app/src/main/java/com/huzaif/habit_tracker/MainActivity.kt
@@ -1,10 +1,22 @@
 package com.huzaif.habit_tracker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.PowerManager
+import android.provider.Settings
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.core.app.ActivityCompat
 import androidx.navigation.compose.rememberNavController
 import com.huzaif.habit_tracker.presentation.navigation.AppNavGraph
@@ -18,20 +30,68 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.POST_NOTIFICATIONS,android.Manifest.permission.USE_EXACT_ALARM), 0)
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.POST_NOTIFICATIONS,
+                    android.Manifest.permission.USE_EXACT_ALARM
+                ),
+                0
+            )
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.SCHEDULE_EXACT_ALARM), 0)
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.SCHEDULE_EXACT_ALARM),
+                0
+            )
         }
 
-//        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.SET_ALARM, android.Manifest.permission.WAKE_LOCK, android.Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS), 0)
 
         setContent {
-            val navController = rememberNavController()
+            val powerManager = getSystemService(POWER_SERVICE) as PowerManager
+            // State to control the visibility of the AlertDialog
+            val showDialog =
+                remember { mutableStateOf(!powerManager.isIgnoringBatteryOptimizations(packageName)) }
+
             Habit_TrackerTheme {
+                // Show AlertDialog only if showDialog is true
+                RequestAlowBackgroundActivityPermission(showDialog)
+
+
+                val navController = rememberNavController()
                 AppNavGraph(navController = navController)
             }
+        }
+    }
+
+    @Composable
+    private fun RequestAlowBackgroundActivityPermission(showDialog: MutableState<Boolean>) {
+        if (showDialog.value) {
+            AlertDialog(
+                onDismissRequest = {
+                    showDialog.value = false
+                    Toast.makeText(
+                        this,
+                        "Please allow background activity to receive reminders",
+                        Toast.LENGTH_LONG
+                    ).show()
+                },
+                title = { Text(text = "Battery Optimization") },
+                text = { Text(text = "To ensure reliable notifications and reminders, please allow background activity for this app. \n\nBattery Usage > Allow background activity") },
+                confirmButton = {
+                    TextButton(onClick = {
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        val uri = Uri.fromParts("package", packageName, null)
+                        intent.data = uri
+                        startActivity(intent)
+                        showDialog.value = false
+                    }) {
+                        Text("Go to Settings")
+                    }
+                }
+            )
         }
     }
 }
